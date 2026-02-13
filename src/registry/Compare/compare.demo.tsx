cc1d022a38cf0b35129e8eda3624e3224962e5dd
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-02-13 12:06:15 +0530

    feat: update CompareDemo layout with improved structure and spacing

--- src/registry/Compare/compare.demo.tsx
+++ src/registry/Compare/compare.demo.tsx
@@ -4,7 +4,8 @@ import { Compare } from "./compare";
 
 export default function CompareDemo() {
   return (
-    <div className="p-4 border rounded-3xl dark:bg-neutral-900 bg-neutral-100  border-neutral-200 dark:border-neutral-800 px-4">
+    <div className="p-4 border rounded-3xl dark:bg-neutral-900 bg-neutral-100 border-neutral-200 dark:border-neutral-800 px-4 mt-4">
+      <div className="flex justify-center items-center">
       <Compare
         firstImage="https://github.com/AbhishekS04/SimpyUI/blob/main/public/code-problem.png?raw=true"
         secondImage="https://github.com/AbhishekS04/SimpyUI/blob/main/public/code-solution.png?raw=true"
@@ -13,6 +14,7 @@ export default function CompareDemo() {
         className="h-[250px] w-[200px] md:h-[500px] md:w-[500px]"
         slideMode="hover"
       />
+      </div>
     </div>
   );
 }
