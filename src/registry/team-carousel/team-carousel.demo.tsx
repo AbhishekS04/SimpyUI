6b3b7da25afaf7ed0a1f83fef6623b8a6eb95288
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-02-15 23:56:57 +0530

    Refactor code structure for improved readability and maintainability

--- src/registry/team-carousel/team-carousel.demo.tsx
+++ src/registry/team-carousel/team-carousel.demo.tsx
@@ -38,7 +38,7 @@ const teamMembers = [
 	{
 		name: "Hulk",
 		role: "DevOps Engineer",
-		image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
+		image: "https://github.com/AbhishekS04/SimpyUI/blob/main/public/Hulk.webp?raw=true",
 		twitter: "https://twitter.com",
 		linkedin: "https://linkedin.com",
 		github: "https://github.com",
